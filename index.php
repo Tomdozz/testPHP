ba68bd1c39942f13fa163c36fc4b7c5fed34dbfd
tomdozz <tom_1995@msn.com>
2018-04-11 09:30:18 +0200

    some small changes

--- index.php
+++ index.php
@@ -7,4 +7,18 @@ use Monolog\Handler\StreamHandler;
 $log = new Logger('Laboration 1');
 $log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
 $log->info("Emil är ful");
-var_dump($argv);
+
+$name = $_GET['name'];
+$log->info($name);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testing, testing</title>
+</head>
+<body>
+<?php
+echo "Hello, " . $name;
+?>
+</body>
+</html>
