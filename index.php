7949441976fce683a375e14b5edeace82ebce659
tomdozz <tom_1995@msn.com>
2018-04-11 09:15:45 +0200

    A decent commit message

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('Laboration 1');
+$log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
+$log->info("Emil är ful");
+var_dump($argv);
